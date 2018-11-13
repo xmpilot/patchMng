5e054558fa31b5caa1e2d6868ebc294326dc524d
苏建锋 <360482507@qq.com>
2018-11-13 16:01:26 +0800

    补丁文件管理工具

---
+++ src/main/java/com/patchmng/main/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.patchmng.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class MainController {
+
+
+}
